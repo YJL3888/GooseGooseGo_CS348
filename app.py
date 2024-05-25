cd59aa15e6d8b86859d9d4f16047b62eb6cb8863
w4zEl <proximitytrigger@gmail.com>
2024-05-25 15:26:15 -0400

    Clean up app.py

--- app.py
+++ app.py
@@ -1,42 +1,29 @@
 import mysql.connector
-from mysql.connector import Error
-# pip install mysql-connector-python
-
 import os
+
 
 def create_connection():
     """Create a database connection to the database"""
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host='goosegoosego.clegmk4ois3q.us-east-1.rds.amazonaws.com',
-            user='admin',
-            password=os.getenv('DB_PASSWORD'),
-            database='sample_db'
-        )
-        print('Connection to MySQL DB successful')
-    except Error as e:
-        print(f"The error '{e}' occurred")
+    return mysql.connector.connect(
+        host='goosegoosego.clegmk4ois3q.us-east-1.rds.amazonaws.com',
+        user='admin',
+        password=os.getenv('DB_PASSWORD'),
+        database='sample_db'
+    )
 
-    return connection
 
-def select_food_items(connection):
+def select_food_items(conn):
     """Query the database and print the results."""
-    cursor = connection.cursor()
-    query = "SELECT * FROM food_items"
-    cursor.execute(query)
-    rows = cursor.fetchall()
+    with conn.cursor() as cursor:
+        query = "SELECT * FROM food_items"
+        cursor.execute(query)
+        rows = cursor.fetchall()
 
-    print('Printing food items')
-    for row in rows:
-        print(row)
-    cursor.close()
+        print('Printing food items')
+        for row in rows:
+            print(row)
 
-def main():
-    connection = create_connection()
-    if connection is not None:
-        select_food_items(connection)
-        connection.close()
 
 if __name__ == '__main__':
-    main()
+    with create_connection() as connection:
+        select_food_items(connection)
